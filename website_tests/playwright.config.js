fe96dfe829cd74ed1c4157291a9090fdb3f9b09f
mace-stev <williamstevenson107@gmail.com>
2026-02-27 08:19:53 -0600

    added github actions workflow

--- website_tests/playwright.config.js
+++ website_tests/playwright.config.js
@@ -71,10 +71,11 @@ module.exports = defineConfig({
   ],
 
 //  /* Run your local dev server before starting the tests */
-//   webServer: {
-//     command: 'npm run start',
-//     url: 'http://127.0.0.1:3000',
-//     reuseExistingServer: !process.env.CI,
-//   },
+  webServer: {
+    command: 'npx http-server ../site -p 4173 -c-1',
+    url: 'http://127.0.0.1:4173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });
 
